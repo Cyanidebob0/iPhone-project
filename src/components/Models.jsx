0262a380cdbdf790b450c36d27f31117fa2cfb95
bhuvan <bhuvanannappa@gmail.com>
2024-10-19 11:37:02 +0530

    models updated

--- src/components/Models.jsx
+++ src/components/Models.jsx
@@ -1,8 +1,22 @@
+import { useGSAP } from '@gsap/react'
 import React from 'react'
+import gsap from 'gsap'
 
 const Models = () => {
+  useGSAP(()=>{
+    gsap.to("#heading",{
+      opacity:1,
+      y:0,
+    })
+  },[])
   return (
-    <div>Models</div>
+    <section className='common-paddign'>
+      <div className='screen-max-width'>
+        <h1 id="heading" className='section-heading'>
+          Take a closer look
+        </h1>
+      </div>
+    </section>
   )
 }
 
