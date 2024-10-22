fd88c312d67abe85a0d8d5362188e1c1aca35f3f
bhuvan <bhuvanannappa@gmail.com>
2024-10-22 10:55:57 +0530

    a

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -12,11 +12,11 @@ const Hero = () => {
       opacity: 1,
       delay: 1.5,
     });
-    gsap.to("#cta",{
-      opacity:1,
-      y:-55,
-      delay:2
-    })
+    gsap.to("#cta", {
+      opacity: 1,
+      y: -55,
+      delay: 2,
+    });
   }, []);
 
   const handleVideoSrcSet = () => {
@@ -42,17 +42,26 @@ const Hero = () => {
           iPhone 15 Pro
         </p>
         <div className="md:w-10/12 w-9/12">
-          <video className="pointer-events-none" autoPlay muted playsInline={true} key={videoSrc}>
+          <video
+            className="pointer-events-none"
+            autoPlay
+            muted
+            playsInline={true}
+            key={videoSrc}
+          >
             <source src={videoSrc} type="video/mp4" />
           </video>
         </div>
       </div>
-      <div id="cta" className="flex flex-col items-center opacity-0 translate-y-20">
-
-        <a href="#highlights" className="btn">buy</a>
-          <p className="font-normal text-xl">From $199/month or $999</p>
+      <div
+        id="cta"
+        className="flex flex-col items-center opacity-0 translate-y-20"
+      >
+        <a href="#highlights" className="btn">
+          buy
+        </a>
+        <p className="font-normal text-xl">From $199/month or $999</p>
       </div>
-
     </section>
   );
 };
